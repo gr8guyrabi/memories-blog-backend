7fe52169d34a2e5a8757900488d63afd2f09fb53
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-03-26 01:15:38 +0545

    setup server

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'   
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = `mongodb+srv://gr8guyrabi:${process.env.MONGODB_KEY}@memories-blog.j7vcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5050
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message))
